dbb333e879163e2a6443f3fb7163a5c21572af9d
cx20 <y-shimano@hondarer-soft.com>
2019-04-26 22:58:26 +0900

    Update three.js examples

---
+++ examples/threejs/teapot/index.js
@@ -0,0 +1,68 @@
+var container;
+var camera, scene, renderer;
+var mesh;
+
+var vertexPositions;
+var vertexNormals;
+var vertexTextureCoords;
+var indices;
+
+function init() {
+    container = document.getElementById('container');
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z = 35;
+    scene = new THREE.Scene();
+
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
+    directionalLight.position.set( 1, 0, 1 );
+    scene.add( directionalLight );
+
+    var geometry = new THREE.BufferGeometry();
+    geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(vertexPositions), 3));
+    geometry.addAttribute('normal', new THREE.BufferAttribute(new Float32Array(vertexNormals), 3));
+    geometry.addAttribute('uv', new THREE.BufferAttribute(new Float32Array(vertexTextureCoords), 2));
+    geometry.setIndex(new THREE.BufferAttribute(new Uint16Array(indices),1));
+    
+    var loader = new THREE.TextureLoader();
+    loader.crossOrigin = 'anonymous';
+    var texture = loader.load('https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/arroway.de_metal+structure+06_d100_flat.jpg');
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    var material = new THREE.MeshLambertMaterial({
+        map: texture
+    });
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0x000000);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
+var angle = 0;
+
+function render() {
+    var axis = new THREE.Vector3(0, 1, 0).normalize();
+    angle += Math.PI / 180;
+    var q = new THREE.Quaternion();
+    q.setFromAxisAngle(axis,angle);
+    mesh.quaternion.copy(q);
+    
+    renderer.render(scene, camera);
+}
+
+$.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+    vertexPositions = data.vertexPositions;
+    vertexTextureCoords = data.vertexTextureCoords;
+    vertexNormals = data.vertexNormals;
+    indices = data.indices;
+    init();
+    animate();
+});
