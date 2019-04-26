941375060c5473240f150b14d6fe262845e90b7a
cx20 <y-shimano@hondarer-soft.com>
2019-04-26 14:26:51 +0900

    Added shree.js examples

---
+++ examples/shreejs/triangle/index.js
@@ -0,0 +1,54 @@
+var container;
+var camera, scene, renderer;
+
+init();
+animate();
+
+function init() {
+    container = document.getElementById('container');
+    camera = new SHREE.Camera();
+    camera.position.z = 2;
+    scene = new SHREE.Scene();
+
+    var vertexPositions = [
+         0.0, 0.5, 0.0, // v0
+        -0.5,-0.5, 0.0, // v1
+         0.5,-0.5, 0.0  // v2
+    ];
+    
+    var vertexColors = [
+        0.0, 0.0, 1.0, 1.0, // v0
+        0.0, 0.0, 1.0, 1.0, // v1
+        0.0, 0.0, 1.0, 1.0  // v2
+    ];
+
+    var geometry = new SHREE.Geometry();
+    geometry.addAttribute('position', 3, vertexPositions);
+    geometry.addAttribute('color', 4, vertexColors);
+    geometry.index = [
+        0, 1, 2 // v0-v1-v2
+    ];
+    
+    var material = new SHREE.Material({
+        vertexShader: document.getElementById('vs').textContent,
+        fragmentShader: document.getElementById('fs').textContent,
+        side: 'SIDE_DOUBLE'
+   });
+
+    var mesh = new SHREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new SHREE.Renderer();
+    renderer.clearColor = [1.0, 1.0, 1.0, 1.0];
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
