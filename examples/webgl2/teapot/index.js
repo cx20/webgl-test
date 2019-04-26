0422dfffafef70f6527c2adacb92a1e28767ddbe
cx20 <y-shimano@hondarer-soft.com>
2019-04-26 21:09:36 +0900

    Update WebGL 2.0 examples

---
+++ examples/webgl2/teapot/index.js
@@ -0,0 +1,139 @@
+var c, gl;
+var aLoc = [];
+var uLoc = [];
+var vertexPositions;
+var vertexNormals;
+var vertexTextureCoords;
+var indices;
+
+function initWebGL() {
+    c = document.getElementById("c");
+    gl = c.getContext("webgl2");
+    gl.enable(gl.DEPTH_TEST);
+    resizeCanvas();
+    window.addEventListener("resize", function(){
+        resizeCanvas();
+    });
+}
+
+function resizeCanvas() {
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
+    gl.viewport(0, 0, c.width, c.height);
+}
+
+function initShaders() {
+    var p = gl.createProgram();
+    var vs = gl.createShader(gl.VERTEX_SHADER);
+    var fs = gl.createShader(gl.FRAGMENT_SHADER);
+    var v = document.getElementById("vs").textContent;
+    var f = document.getElementById("fs").textContent;
+    gl.shaderSource(vs, v);
+    gl.shaderSource(fs, f);
+    gl.compileShader(vs);
+    gl.compileShader(fs);
+    console.log(gl.getShaderInfoLog(vs));
+    console.log(gl.getShaderInfoLog(fs));
+    gl.attachShader(p, vs);
+    gl.attachShader(p, fs);
+    gl.linkProgram(p);
+    gl.useProgram(p);
+    aLoc[0] = gl.getAttribLocation(p, "position");
+    aLoc[1] = gl.getAttribLocation(p, "normal");
+    aLoc[2] = gl.getAttribLocation(p, "textureCoord");
+    uLoc[0] = gl.getUniformLocation(p, "uPMatrix");
+    uLoc[1] = gl.getUniformLocation(p, "uMVMatrix");
+    uLoc[2]  = gl.getUniformLocation(p, "uTexture");
+    uLoc[3]  = gl.getUniformLocation(p, "uPointLightingLocation");
+    gl.enableVertexAttribArray(aLoc[0]);
+    gl.enableVertexAttribArray(aLoc[1]);
+    gl.enableVertexAttribArray(aLoc[2]);
+}
+
+var mvMatrix = mat4.create();
+var pMatrix = mat4.create();
+
+var vertexPositionBuffer;
+var vertexNormalBuffer;
+var vertexCoordBuffer;
+var vertexIndexBuffer;
+
+function initBuffers() {
+    vertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexPositions), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(aLoc[0], 3, gl.FLOAT, false, 0, 0);
+
+    vertexNormalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(aLoc[1], 3, gl.FLOAT, false, 0, 0);
+    
+    vertexCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexTextureCoords), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(aLoc[2], 2, gl.FLOAT, false, 0, 0);
+
+    vertexIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+    var img = new Image();
+    var texture;
+    img.onload = function(){
+        texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+        gl.generateMipmap(gl.TEXTURE_2D);
+    };
+    img.crossOrigin = "anonymous";
+    img.src = "https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/arroway.de_metal+structure+06_d100_flat.jpg";
+}
+
+var rad = 0;
+function draw() {
+
+    rad += Math.PI * 1.0 / 180.0;
+    mat4.perspective(pMatrix, 45, c.width / c.height, 0.1, 100.0);
+    mat4.identity(mvMatrix);
+    var translation = vec3.create();
+    vec3.set(translation, 0.0, 0.0, -35.0);
+    mat4.translate(mvMatrix, mvMatrix, translation);
+    mat4.rotate(mvMatrix, mvMatrix, rad, [0, 1, 0]);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+    gl.vertexAttribPointer(aLoc[0], 3, gl.FLOAT, false, 0, 0);
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer);
+    gl.vertexAttribPointer(aLoc[1], 3, gl.FLOAT, false, 0, 0);
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexCoordBuffer);
+    gl.vertexAttribPointer(aLoc[2], 2, gl.FLOAT, false, 0, 0);
+    
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
+    gl.uniformMatrix4fv(uLoc[0], false, pMatrix);
+    gl.uniformMatrix4fv(uLoc[1], false, mvMatrix);
+
+    var lightDirection = [1.0, 1.0, -1.0];
+    gl.uniform3fv(uLoc[2], lightDirection);
+
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+    gl.flush();
+}
+
+function animate() {
+    draw();
+    requestAnimationFrame(animate);
+}
+
+$.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+    vertexPositions = data.vertexPositions;
+    vertexTextureCoords = data.vertexTextureCoords;
+    vertexNormals = data.vertexNormals;
+    indices = data.indices;
+    initWebGL();
+    initShaders();
+    initBuffers();
+    animate();
+});
+
