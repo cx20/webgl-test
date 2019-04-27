e07bfb3b777a67e29d4b252b5615c88868461d35
cx20 <y-shimano@hondarer-soft.com>
2019-04-27 15:06:11 +0900

    Update picogl.js examples

--- examples/picogl/teapot/index.js
+++ examples/picogl/teapot/index.js
@@ -18,53 +18,55 @@ var program = app.createProgram(vsSource, fsSource);
 
 var image = new Image();
 image.src = "../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg";
+image.onload = function() {
 
-$.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
-    vertexPositions = data.vertexPositions;
-    vertexTextureCoords = data.vertexTextureCoords;
-    vertexNormals = data.vertexNormals;
-    indices = data.indices;
+    $.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+        vertexPositions = data.vertexPositions;
+        vertexTextureCoords = data.vertexTextureCoords;
+        vertexNormals = data.vertexNormals;
+        indices = data.indices;
 
-    var texture = app.createTexture2D(image);
+        var texture = app.createTexture2D(image);
 
-    var positions = app.createVertexBuffer(PicoGL.FLOAT, 3, new Float32Array(vertexPositions));
-    var textureCoords = app.createVertexBuffer(PicoGL.FLOAT, 2, new Float32Array(vertexTextureCoords));
-    var normals = app.createVertexBuffer(PicoGL.FLOAT, 3, new Float32Array(vertexNormals));
-    var indices_ = app.createIndexBuffer(PicoGL.UNSIGNED_SHORT, 3, new Uint16Array(indices));
+        var positions = app.createVertexBuffer(PicoGL.FLOAT, 3, new Float32Array(vertexPositions));
+        var textureCoords = app.createVertexBuffer(PicoGL.FLOAT, 2, new Float32Array(vertexTextureCoords));
+        var normals = app.createVertexBuffer(PicoGL.FLOAT, 3, new Float32Array(vertexNormals));
+        var indices_ = app.createIndexBuffer(PicoGL.UNSIGNED_SHORT, 3, new Uint16Array(indices));
 
-    var vertexArray = app.createVertexArray()
-        .vertexAttributeBuffer(0, positions)
-        .vertexAttributeBuffer(1, textureCoords)
-        .vertexAttributeBuffer(2, normals)
-        .indexBuffer(indices_);
+        var vertexArray = app.createVertexArray()
+            .vertexAttributeBuffer(0, positions)
+            .vertexAttributeBuffer(1, textureCoords)
+            .vertexAttributeBuffer(2, normals)
+            .indexBuffer(indices_);
 
-    var drawCall = app.createDrawCall(program, vertexArray)
-        .texture("texture", texture);
+        var drawCall = app.createDrawCall(program, vertexArray)
+            .texture("texture", texture);
 
-    window.onresize = function() {
-        app.resize(window.innerWidth, window.innerHeight);
-    };
+        window.onresize = function() {
+            app.resize(window.innerWidth, window.innerHeight);
+        };
 
-    var mvMatrix = mat4.create();
-    var pMatrix = mat4.create();
+        var mvMatrix = mat4.create();
+        var pMatrix = mat4.create();
 
-    var rad = 0;
-    function draw() {
-        rad += Math.PI * 1.0 / 180.0;
-        mat4.perspective(pMatrix, 45, window.innerWidth / window.innerHeight, 0.1, 100.0);
-        mat4.identity(mvMatrix);
-        var translation = vec3.create();
-        vec3.set(translation, 0.0, 0.0, -30.0);
-        mat4.translate(mvMatrix, mvMatrix, translation);
-        mat4.rotate(mvMatrix, mvMatrix, rad, [0, 1, 0]);
+        var rad = 0;
+        function draw() {
+            rad += Math.PI * 1.0 / 180.0;
+            mat4.perspective(pMatrix, 45, window.innerWidth / window.innerHeight, 0.1, 100.0);
+            mat4.identity(mvMatrix);
+            var translation = vec3.create();
+            vec3.set(translation, 0.0, 0.0, -30.0);
+            mat4.translate(mvMatrix, mvMatrix, translation);
+            mat4.rotate(mvMatrix, mvMatrix, rad, [0, 1, 0]);
 
-        drawCall.uniform("uPMatrix", pMatrix);
-        drawCall.uniform("uMVMatrix", mvMatrix);
+            drawCall.uniform("uPMatrix", pMatrix);
+            drawCall.uniform("uMVMatrix", mvMatrix);
 
-        app.clear();
-        drawCall.draw();
-        requestAnimationFrame(draw);
-    }
+            app.clear();
+            drawCall.draw();
+            requestAnimationFrame(draw);
+        }
 
-    draw();
-});
+        draw();
+    });
+}
