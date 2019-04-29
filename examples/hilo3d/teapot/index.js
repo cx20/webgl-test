f799ec4309436d321ade518bf1783bcc4a32c903
cx20 <y-shimano@hondarer-soft.com>
2019-04-30 00:13:12 +0900

    Update Hilo3d examples

--- examples/hilo3d/teapot/index.js
+++ examples/hilo3d/teapot/index.js
@@ -17,7 +17,8 @@ var vertexNormals;
 var vertexTextureCoords;
 var indices;
 
-$.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+// copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/Teapot.json
+$.getJSON("../../../assets/json/teapot.json", function (data) {
     vertexPositions = data.vertexPositions;
     vertexTextureCoords = data.vertexTextureCoords;
     vertexNormals = data.vertexNormals;
@@ -38,12 +39,18 @@ $.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d8
         material: new Hilo3d.ShaderMaterial({
             cullFace: false,
             diffuse: new Hilo3d.LazyTexture({
+                // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/arroway.de_metal%2Bstructure%2B06_d100_flat.jpg
                 src: '../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg'
             }),
             uniforms:{
                 u_modelViewProjectionMatrix: 'MODELVIEWPROJECTION',
                 u_modelViewMatrix: 'MODELVIEW',
                 u_diffuse: 'DIFFUSE',
+                uPointLightingLocation: {
+                    get: function() {
+                        return [100.0, 0, 100.0];
+                    }
+                }
             },
             attributes:{
                 a_position: 'POSITION',
@@ -54,9 +61,7 @@ $.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d8
             vs: document.getElementById('vs').textContent
         }),
         onUpdate: function() {
-           //this.rotationX += 0.5;
            this.rotationY += 0.5;
-           //this.rotationZ += 0.5;
         }
     });
 
