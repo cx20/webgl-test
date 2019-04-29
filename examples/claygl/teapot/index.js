7e1cf23d7d1f1c53ba104bb52ff36ba00072d42a
cx20 <y-shimano@hondarer-soft.com>
2019-04-30 00:06:17 +0900

    Update ClayGL examples

--- examples/claygl/teapot/index.js
+++ examples/claygl/teapot/index.js
@@ -3,7 +3,8 @@ var vertexNormals;
 var vertexTextureCoords;
 var indices;
 
-$.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+// copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/Teapot.json
+$.getJSON("../../../assets/json/teapot.json", function (data) {
     vertexPositions = data.vertexPositions;
     vertexTextureCoords = data.vertexTextureCoords;
     vertexNormals = data.vertexNormals;
@@ -26,16 +27,16 @@ $.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d8
                 shader: new clay.Shader(vs, fs)
             });
             var diffuse = new clay.Texture2D;
+            // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/arroway.de_metal%2Bstructure%2B06_d100_flat.jpg
             diffuse.load("../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg");
             material.set('texture', diffuse);
+            material.set('uPointLightingLocation', [100.0, 0.0, 100.0]);
             this._mesh = app.createMesh(geometry, material);
             this._mesh.culling = false;
         },
 
         loop: function () {
-            //this._mesh.rotation.rotateX(app.frameTime / 1000);
             this._mesh.rotation.rotateY(app.frameTime / 1000);
-            //this._mesh.rotation.rotateZ(app.frameTime / 1000);
         }
     });
 });
