c2dc16477a128366ff31eaf1e2be29ff468f5c99
cx20 <y-shimano@hondarer-soft.com>
2019-04-30 00:17:40 +0900

    Update PlayCanvas examples

--- examples/playcanvas/teapot/index.js
+++ examples/playcanvas/teapot/index.js
@@ -10,8 +10,6 @@ app.start();
 app.on("update", function (dt) {
 });
 
-//app.scene.ambientLight = new pc.Color(1, 1, 1);
-
 var camera = new pc.Entity();
 camera.addComponent('camera', {
     clearColor: new pc.Color(0, 0, 0),
@@ -36,7 +34,8 @@ var vertexNormals;
 var vertexTextureCoords;
 var indices;
 
-$.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+// copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/Teapot.json
+$.getJSON("../../../assets/json/teapot.json", function (data) {
     vertexPositions = data.vertexPositions;
     vertexTextureCoords = data.vertexTextureCoords;
     vertexNormals = data.vertexNormals;
@@ -67,6 +66,7 @@ $.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d8
                 texture.addressV = pc.gfx.ADDRESS_REPEAT;
                 texture.setSource(img);
             };
+            // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/arroway.de_metal%2Bstructure%2B06_d100_flat.jpg
             img.src = "../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg";
             return texture;
         }
