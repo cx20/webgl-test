6cf5fc082f8e0b34ccdfff6b335510b0ac1e246b
cx20 <y-shimano@hondarer-soft.com>
2019-04-29 23:25:00 +0900

    Update TWGL examples

--- examples/twgl/teapot/index.js
+++ examples/twgl/teapot/index.js
@@ -51,7 +51,8 @@ function render() {
     requestAnimationFrame(render);
 }
 
-$.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+// copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/Teapot.json
+$.getJSON("../../../assets/json/teapot.json", function (data) {
     vertexPositions = data.vertexPositions;
     vertexTextureCoords = data.vertexTextureCoords;
     vertexNormals = data.vertexNormals;
@@ -67,7 +68,7 @@ $.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d8
     bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
 
     var tex = twgl.createTexture(gl, {
-        crossOrigin: "anonymous",
+        // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/arroway.de_metal%2Bstructure%2B06_d100_flat.jpg
         src: "../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg"
     });
 
