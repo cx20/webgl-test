184a39171ca49155bb2973e97ae0cf4c8f9be8de
cx20 <y-shimano@hondarer-soft.com>
2019-04-30 00:24:40 +0900

    Update CZPG examples

--- examples/czpg/teapot/index.js
+++ examples/czpg/teapot/index.js
@@ -2,11 +2,12 @@ const renderer = new CZPG.Renderer('c').setSize('100%', '100%');
 const context = renderer.context;
 const scene = new CZPG.Scene(renderer);
 
-let camera = new CZPG.PerspectiveCamera(45, context.canvas.width/context.canvas.height, 0.01, 2000);
+let camera = new CZPG.PerspectiveCamera(45, context.canvas.width / context.canvas.height, 0.01, 1000);
 camera.transform.position = [0, 1, 50];
 camera.updateViewMatrix();
 
-$.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+// copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/Teapot.json
+$.getJSON("../../../assets/json/teapot.json", function (data) {
     const vertexPositions = data.vertexPositions;
     const vertexTextureCoords = data.vertexTextureCoords;
     const vertexNormals = data.vertexNormals;
@@ -16,6 +17,7 @@ $.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d8
         indices: { data: indices },
     };
     const textures = CZPG.createTextures(context, {pic: {
+        // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/arroway.de_metal%2Bstructure%2B06_d100_flat.jpg
         src:"../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg",
         min : context.LINEAR_MIPMAP_LINEAR,
         mag : context.LINEAR}});
@@ -23,9 +25,12 @@ $.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d8
     attribArrays['a_position'] = { data: vertexPositions, numComponents: 3 };
     attribArrays['a_normal'] = { data: vertexNormals, numComponents: 3 };
     attribArrays['a_textureCoord'] = { data: vertexTextureCoords, numComponents: 2 };
-    var mesh = new CZPG.Mesh('quad', attribArrays, {cullFace: false});
-    var model = new CZPG.Model(mesh);
-    var shader = new CZPG.Shader(context, 'vs', 'fs').setCamera(camera).setUniformObj({u_texture: textures.pic});
+    const mesh = new CZPG.Mesh('quad', attribArrays, {cullFace: false});
+    const model = new CZPG.Model(mesh);
+    const shader = new CZPG.Shader(context, 'vs', 'fs').setCamera(camera).setUniformObj({
+        u_texture: textures.pic,
+        u_pointLightingLocation: [100.0, 0.0, 100.0]
+    });
 
     scene.add({shader: shader, model: model});
 
