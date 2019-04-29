f6b2a3b9ebebb3b65854df6c41f85c479fb78d45
cx20 <y-shimano@hondarer-soft.com>
2019-04-30 08:16:54 +0900

    Update filament examples

--- examples/filament/teapot/index.js
+++ examples/filament/teapot/index.js
@@ -113,13 +113,15 @@ class App {
   }
 }
 
-$.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+// copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/Teapot.json
+$.getJSON("../../../assets/json/teapot.json", function (data) {
     vertexPositions = data.vertexPositions;
     vertexTextureCoords = data.vertexTextureCoords;
     vertexNormals = data.vertexNormals;
     indices = data.indices;
 
     Filament.init([
+        // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/arroway.de_metal%2Bstructure%2B06_d100_flat.jpg
         '../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg',
         'teapot.filamat'
     ], () => {
