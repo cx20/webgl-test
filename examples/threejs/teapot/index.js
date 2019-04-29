b936c35900e648add688d0a02ff18651bb20d084
cx20 <y-shimano@hondarer-soft.com>
2019-04-29 23:20:28 +0900

    Update three.js examples

--- examples/threejs/teapot/index.js
+++ examples/threejs/teapot/index.js
@@ -24,7 +24,7 @@ function init() {
     geometry.setIndex(new THREE.BufferAttribute(new Uint16Array(indices),1));
     
     var loader = new THREE.TextureLoader();
-    loader.crossOrigin = 'anonymous';
+    // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/arroway.de_metal%2Bstructure%2B06_d100_flat.jpg
     var texture = loader.load('../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg');
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
@@ -58,7 +58,8 @@ function render() {
     renderer.render(scene, camera);
 }
 
-$.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+// copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/Teapot.json
+$.getJSON("../../../assets/json/teapot.json", function (data) {
     vertexPositions = data.vertexPositions;
     vertexTextureCoords = data.vertexTextureCoords;
     vertexNormals = data.vertexNormals;
