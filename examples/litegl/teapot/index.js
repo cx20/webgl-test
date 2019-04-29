c5451e69b107e8353df7a242ba47fe684073a1dd
cx20 <y-shimano@hondarer-soft.com>
2019-04-29 23:41:25 +0900

    Update litegl.js examples

--- examples/litegl/teapot/index.js
+++ examples/litegl/teapot/index.js
@@ -66,7 +66,8 @@ gl.onupdate = function(dt)
     mat4.rotateY(model,model, dt * 0.5);
 };
 
-$.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+// copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/Teapot.json
+$.getJSON("../../../assets/json/teapot.json", function (data) {
     vertexPositions = data.vertexPositions;
     vertexTextureCoords = data.vertexTextureCoords;
     vertexNormals = data.vertexNormals;
@@ -79,6 +80,8 @@ $.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d8
         wrapS: gl.REPEAT,
         wrapT: gl.REPEAT
     };
+
+    // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/arroway.de_metal%2Bstructure%2B06_d100_flat.jpg
     texture = GL.Texture.fromURL("../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg", options);
 
     gl.animate();
