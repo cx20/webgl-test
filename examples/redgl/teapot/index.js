30c6733c48a1b301def01e4561865335915ce3fc
cx20 <y-shimano@hondarer-soft.com>
2019-04-30 00:44:41 +0900

    Update RedGL examples

--- examples/redgl/teapot/index.js
+++ examples/redgl/teapot/index.js
@@ -19,7 +19,8 @@ RedGL(canvas, function (v) {
     var tInterleaveBuffer, tIndexBuffer;
     var self = this;
 
-    $.getJSON("https://rawcdn.githack.com/gpjt/webgl-lessons/a227a62af468272a06d55d815971273628874067/lesson14/Teapot.json", function (data) {
+    // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/Teapot.json
+    $.getJSON("../../../assets/json/teapot.json", function (data) {
         vertexPositions = data.vertexPositions;
         vertexTextureCoords = data.vertexTextureCoords;
         vertexNormals = data.vertexNormals;
@@ -58,14 +59,15 @@ RedGL(canvas, function (v) {
             indexData  // data
         );
         
+        // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/arroway.de_metal%2Bstructure%2B06_d100_flat.jpg
         var tTexture = RedBitmapTexture(self, '../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg');
         var tMaterial = RedStandardMaterial(self, tTexture);
         
         var tDLight = RedDirectionalLight(self);
-        tScene.addLight(tDLight);
         tDLight.x = 1;
         tDLight.y = 0;
         tDLight.z = 1;
+        tScene.addLight(tDLight);
 
         tGeometry = RedGeometry(tInterleaveBuffer, tIndexBuffer);
         tMesh = RedMesh(
