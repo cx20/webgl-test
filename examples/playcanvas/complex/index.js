4a370041b65925a181e96c0e57d07c863d11d15f
cx20 <y-shimano@hondarer-soft.com>
2020-04-09 00:43:59 +0900

    [WIP] Added a composite sample using PlayCanvas

---
+++ examples/playcanvas/complex/index.js
@@ -0,0 +1,361 @@
+let modelInfoSet = [
+{
+    name: "CesiumMilkTruck",
+    scale: 0.4,
+    rotation: [0, 0, 0],
+    position: [0, 0, -2],
+    url: "https://cx20.github.io/gltf-test/sampleModels/CesiumMilkTruck/glTF/CesiumMilkTruck.gltf"
+}, {
+    name: "Fox",
+    scale: 0.05,
+    rotation: [0, Math.PI / 2, 0],
+    position: [0, 0, 0],
+    url: "https://cx20.github.io/gltf-test/sampleModels/Fox/glTF/Fox.gltf"
+}, {
+    name: "Rex",
+    scale: 0.01,
+    rotation: [0, Math.PI / 2, 0],
+    position: [0, 0, 3],
+    url: "https://rawcdn.githack.com/BabylonJS/Exporters/9bc140006be149687be045f60b4a25cdb45ce4fc/Maya/Samples/glTF 2.0/T-Rex/trex_running.gltf"
+}];
+
+let decoderModule;
+
+var getAbsolutePathFromRelativePath = function(href) {
+    var link = document.createElement("a");
+    link.href = href;
+    return link.href;
+}
+
+var Viewer = function (canvas) {
+
+    var self = this;
+
+    // create the application
+    var app = new pc.Application(canvas, {
+        mouse: new pc.Mouse(canvas),
+        touch: new pc.TouchDevice(canvas)
+    });
+
+    var getCanvasSize = function () {
+        return {
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
+    };
+
+    app.graphicsDevice.maxPixelRatio = window.devicePixelRatio;
+
+    // Set the canvas to fill the window and automatically change resolution to be the same as the canvas size
+    var canvasSize = getCanvasSize();
+    app.setCanvasFillMode(pc.FILLMODE_NONE, canvasSize.width, canvasSize.height);
+    app.setCanvasResolution(pc.RESOLUTION_AUTO);
+    window.addEventListener("resize", function () {
+        var canvasSize = getCanvasSize();
+        app.resizeCanvas(canvasSize.width, canvasSize.height);
+    });
+
+    // set a prefiltered cubemap as the skybox
+    let cubemapAsset = new pc.Asset('papermill', 'cubemap', {
+        url: "https://cx20.github.io/gltf-test/textures/dds/papermill.dds"
+    }, {
+        "textures": [
+            "https://cx20.github.io/gltf-test/textures/papermill/specular/specular_right_0.jpg",
+            "https://cx20.github.io/gltf-test/textures/papermill/specular/specular_left_0.jpg",
+            "https://cx20.github.io/gltf-test/textures/papermill/specular/specular_top_0.jpg",
+            "https://cx20.github.io/gltf-test/textures/papermill/specular/specular_bottom_0.jpg",
+            "https://cx20.github.io/gltf-test/textures/papermill/specular/specular_front_0.jpg",
+            "https://cx20.github.io/gltf-test/textures/papermill/specular/specular_back_0.jpg",
+        ],
+        "magFilter": 1,
+        "minFilter": 5,
+        "anisotropy": 1,
+        "name": "Papermill",
+        "rgbm": true,
+        "prefiltered": "papermill.dds"
+    });
+    cubemapAsset.ready(function () {
+        app.scene.gammaCorrection = pc.GAMMA_SRGB;
+        app.scene.toneMapping = pc.TONEMAP_ACES;
+        app.scene.skyboxMip = 1;                        // Set the skybox to the 128x128 cubemap mipmap level
+        app.scene.setSkybox(cubemapAsset.resources);
+    });
+    app.assets.add(cubemapAsset);
+    app.assets.load(cubemapAsset);
+
+    // create the orbit camera
+    var camera = new pc.Entity("Camera");
+    camera.addComponent("camera", {
+        fov: 60,
+        clearColor: new pc.Color(0.4, 0.45, 0.5)
+    });
+
+    // load orbit script
+    app.assets.loadFromUrl(
+        "https://cx20.github.io/gltf-test/libs/playcanvas/v1.27.0-dev/orbit-camera.js",
+        "script",
+        function (err, asset) {
+            // setup orbit script component
+            camera.addComponent("script");
+            camera.script.create("orbitCamera", {
+                attributes: {
+                    inertiaFactor: 0.1
+                }
+            });
+            camera.script.create("orbitCameraInputMouse");
+            camera.script.create("orbitCameraInputTouch");
+            app.root.addChild(camera);
+        });
+
+    // create the light
+    var light = new pc.Entity();
+    light.addComponent("light", {
+        type: "directional",
+        color: new pc.Color(1, 1, 1),
+        castShadows: true,
+        intensity: 2,
+        shadowBias: 0.2,
+        shadowDistance: 5,
+        normalOffsetBias: 0.05,
+        shadowResolution: 2048
+    });
+    light.setLocalEulerAngles(45, 30, 0);
+    app.root.addChild(light);
+
+    app.start();
+
+    var graph = new Graph(app, 128);
+    app.on('prerender', function () {
+        if (self.showGraphs) {
+            graph.update();
+            graph.render();
+        }
+    });
+
+    // store things
+    this.app = app;
+    this.camera = camera;
+    this.light = light;
+    this.entity = null;
+    this.graph = graph;
+    this.showGraphs = false;
+
+    for (let i = 0; i < modelInfoSet.length; i++) {
+        let m = modelInfoSet[i];
+        let url = m.url;
+        var filename = url.split('/').pop();
+        self.load(filename, url);
+    }
+};
+
+Object.assign(Viewer.prototype, {
+    // reset the viewer, unloading resources
+    resetScene: function () {
+        var app = this.app;
+
+        var entity = this.entity;
+        if (entity) {
+            app.root.removeChild(entity);
+            entity.destroy();
+            this.entity = null;
+        }
+
+        if (this.asset) {
+            app.assets.remove(this.asset);
+            this.asset.unload();
+            this.asset = null;
+        }
+
+        this.graph.clear();
+
+        this.animationMap = { };
+        //onAnimationsLoaded([]);
+    },
+
+    // move the camera to view the loaded object
+    focusCamera: function () {
+        var entity = this.entity;
+        if (entity) {
+            var camera = this.camera;
+
+            var orbitCamera = camera.script.orbitCamera;
+            orbitCamera.focus(entity);
+
+            var distance = orbitCamera.distance;
+            camera.camera.nearClip = distance / 10;
+            camera.camera.farClip = distance * 10;
+
+            var light = this.light;
+            light.light.shadowDistance = distance * 2;
+        }
+    },
+
+    // load model from the url
+    load: function (filename, url) {
+        this.app.assets.loadFromUrl(url, "container", this._onLoaded.bind(this), filename);
+    },
+
+    // play the animation
+    play: function (animationName) {
+        if (animationName) {
+            this.entity.animation.play(this.animationMap[animationName], 1);
+        } else {
+            this.entity.animation.playing = true;
+        }
+    },
+
+    // stop playing animations
+    stop: function () {
+        this.entity.animation.playing = false;
+    },
+
+    setSpeed: function (speed) {
+        var entity = this.entity;
+        if (entity) {
+            entity.animation.speed = speed;
+        }
+    },
+
+    setGraphs: function (show) {
+        this.showGraphs = show;
+    },
+
+    _onLoaded: function (err, asset) {
+        if (!err) {
+
+            //this.resetScene();
+
+            var resource = asset.resource;
+
+            // create entity and add model
+            var entity = new pc.Entity();
+            entity.addComponent("model", {
+                type: "asset",
+                asset: resource.model,
+                castShadows: true
+            });
+            	
+            if (resource.model.name == "CesiumMilkTruck.gltf/model/0") {
+                let m = modelInfoSet[0];
+                applyModelInfoToEntity(m, entity);
+            } else if (resource.model.name == "Fox.gltf/model/0") {
+                let m = modelInfoSet[1];
+                applyModelInfoToEntity(m, entity);
+            } else if (resource.model.name == "trex_running.gltf/model/0") {
+                let m = modelInfoSet[2];
+                applyModelInfoToEntity(m, entity);
+            }
+            function applyModelInfoToEntity(m, entity) { 
+                var s = entity.getLocalScale();
+                var r = entity.getLocalRotation();
+                var p = entity.getLocalPosition();
+                s.x = s.x * m.scale;
+                s.y = s.y * m.scale;
+                s.z = s.z * m.scale;
+                r.setFromEulerAngles(
+                    m.rotation[0] / (2 * Math.PI) * 360,
+                    m.rotation[1] / (2 * Math.PI) * 360,
+                    m.rotation[2] / (2 * Math.PI) * 360);
+                p.x = p.x + m.position[0];
+                p.y = p.y + m.position[1];
+                p.z = p.z + m.position[2];
+                entity.setLocalScale(s);
+                entity.setLocalRotation(r);
+                entity.setLocalPosition(p);
+            }
+
+            // create animations
+            if (resource.animations && resource.animations.length > 0) {
+                entity.addComponent('animation', {
+                    assets: resource.animations.map(function (asset) {
+                        return asset.id;
+                    }),
+                    speed: 1
+                });
+
+                var animationMap = {};
+                for (var i = 0; i < resource.animations.length; ++i) {
+                    var animAsset = resource.animations[i];
+                    animationMap[animAsset.resource.name] = animAsset.name;
+                }
+
+                this.animationMap = animationMap;
+                //onAnimationsLoaded(Object.keys(this.animationMap));
+
+                var createAnimGraphs = function () {
+                    var extract = function (index) {
+                        return this[index];
+                    };
+
+                    var graph = this.graph;
+                    var animController = entity.animation.data.animController;
+                    var nodes = animController._nodes;
+                    var activePose = animController._activePose;
+
+                    for (var i = 0; i < nodes.length; ++i) {
+                        var node = nodes[i];
+
+                        graph.addGraph(node, new pc.Color(1, 0, 0, 1), extract.bind(activePose, i * 10 + 0));
+                        graph.addGraph(node, new pc.Color(0, 1, 0, 1), extract.bind(activePose, i * 10 + 1));
+                        graph.addGraph(node, new pc.Color(0, 0, 1, 1), extract.bind(activePose, i * 10 + 2));
+
+                        graph.addGraph(node, new pc.Color(1, 0, 0, 1), extract.bind(activePose, i * 10 + 3));
+                        graph.addGraph(node, new pc.Color(0, 1, 0, 1), extract.bind(activePose, i * 10 + 4));
+                        graph.addGraph(node, new pc.Color(0, 0, 1, 1), extract.bind(activePose, i * 10 + 5));
+                        graph.addGraph(node, new pc.Color(1, 1, 0, 1), extract.bind(activePose, i * 10 + 6));
+
+                        graph.addGraph(node, new pc.Color(1, 0, 0, 1), extract.bind(activePose, i * 10 + 7));
+                        graph.addGraph(node, new pc.Color(0, 1, 0, 1), extract.bind(activePose, i * 10 + 8));
+                        graph.addGraph(node, new pc.Color(0, 0, 1, 1), extract.bind(activePose, i * 10 + 9));
+                    }
+                };
+
+                // create animation graphs
+                setTimeout(createAnimGraphs.bind(this), 1000);
+            }
+
+            this.app.root.addChild(entity);
+            this.entity = entity;
+            this.asset = asset;
+
+            this.focusCamera();
+        }
+    }
+});
+
+var viewer;
+
+function main(){
+    if (true) {//typeof WebAssembly !== 'object') {
+        loadScript('https://cx20.github.io/gltf-test/libs/playcanvas/v1.27.0-dev/draco_decoder.js').then(function () {
+            decoderModule = DracoDecoderModule();
+            onLoad();
+        });
+    } else {
+        loadScript('https://cx20.github.io/gltf-test/libs/playcanvas/v1.27.0-dev/draco_wasm_wrapper.js').then(function () {
+            fetch('https://cx20.github.io/gltf-test/libs/playcanvas/v1.27.0-dev/draco_decoder.wasm').then(function (response) {
+                response.arrayBuffer().then(function (arrayBuffer) {
+                    decoderModule = DracoDecoderModule({ wasmBinary: arrayBuffer });
+                    onLoad();
+                });
+            });
+        });
+    }
+}
+
+function loadScript(src) {
+    let head = document.getElementsByTagName('head')[0];
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    return new Promise(function (resolve) {
+        script.onload = resolve;
+        head.appendChild(script);
+    });
+}
+
+function onLoad() {
+    viewer = new Viewer(document.getElementById("application"));
+}
+
+main();
