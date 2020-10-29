7b60bf42ba532c772544359219dcdeae77838781
cx20 <y-shimano@hondarer-soft.com>
2020-10-30 05:14:14 +0900

    Update three.js r121 to r122

--- examples/threejs/texture/index.js
+++ examples/threejs/texture/index.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cx20.github.io/gltf-test/libs/three.js/r121/build/three.module.js';
+import * as THREE from 'https://cx20.github.io/gltf-test/libs/three.js/r122/build/three.module.js';
 
 let container;
 let camera, scene, renderer;
