27f1fac64227358fa3d8680b400d019b702dc76f
cx20 <y-shimano@hondarer-soft.com>
2020-12-06 14:03:33 +0900

    Update Babylon.js v4.2.0 to v5.0.0alpha

--- examples/babylonjs/teapot/index.js
+++ examples/babylonjs/teapot/index.js
@@ -1,42 +1,49 @@
-let teapotMesh;
+function init() {
+    const canvas = document.querySelector("#c");
+    const engine = new BABYLON.Engine(canvas);
 
-let vertexPositions;
-let vertexNormals;
-let vertexTextureCoords;
-let indices;
+    let teapotMesh;
+    let vertexPositions;
+    let vertexNormals;
+    let vertexTextureCoords;
+    let indices;
 
-let createScene = function(engine) {
-    let scene = new BABYLON.Scene(engine);
-    let camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0, -50), scene);
-    let light1 = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(1.0, 0.0, 1.0), scene);
-    scene.clearColor = new BABYLON.Color3(0, 0, 0);
-    let material = new BABYLON.StandardMaterial("material", scene);
-    // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/arroway.de_metal%2Bstructure%2B06_d100_flat.jpg
-    material.diffuseTexture = new BABYLON.Texture("../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg", scene);
-    teapotMesh = new BABYLON.Mesh("teapot", scene);
-    teapotMesh.material = material;
-    teapotMesh.material.backFaceCulling = false;
-    teapotMesh.setVerticesData(BABYLON.VertexBuffer.PositionKind, vertexPositions, false);
-    teapotMesh.setVerticesData(BABYLON.VertexBuffer.NormalKind, vertexNormals, false);
-    teapotMesh.setVerticesData(BABYLON.VertexBuffer.UVKind, vertexTextureCoords, false);
-    teapotMesh.setIndices(indices);
+    const createScene = function(engine) {
+        const scene = new BABYLON.Scene(engine);
+        const camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0, -50), scene);
+        const light1 = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(1.0, 0.0, 1.0), scene);
+        scene.clearColor = new BABYLON.Color3(0, 0, 0);
+        const material = new BABYLON.StandardMaterial("material", scene);
+        // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/arroway.de_metal%2Bstructure%2B06_d100_flat.jpg
+        material.diffuseTexture = new BABYLON.Texture("../../../assets/textures/arroway.de_metal+structure+06_d100_flat.jpg", scene);
+        teapotMesh = new BABYLON.Mesh("teapot", scene);
+        teapotMesh.material = material;
+        teapotMesh.material.backFaceCulling = false;
+        teapotMesh.setVerticesData(BABYLON.VertexBuffer.PositionKind, vertexPositions, false);
+        teapotMesh.setVerticesData(BABYLON.VertexBuffer.NormalKind, vertexNormals, false);
+        teapotMesh.setVerticesData(BABYLON.VertexBuffer.UVKind, vertexTextureCoords, false);
+        teapotMesh.setIndices(indices);
 
-    return scene;
-};
+        return scene;
+    };
 
-// copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/Teapot.json
-$.getJSON("../../../assets/json/teapot.json", function (data) {
-    vertexPositions = data.vertexPositions;
-    vertexTextureCoords = data.vertexTextureCoords;
-    vertexNormals = data.vertexNormals;
-    indices = data.indices;
-    let canvas = document.querySelector("#c");
-    let engine = new BABYLON.Engine(canvas, true);
-    let scene = createScene(engine);
+    // copy from: https://github.com/gpjt/webgl-lessons/blob/master/lesson14/Teapot.json
+    $.getJSON("../../../assets/json/teapot.json", function (data) {
+        vertexPositions = data.vertexPositions;
+        vertexTextureCoords = data.vertexTextureCoords;
+        vertexNormals = data.vertexNormals;
+        indices = data.indices;
+        const scene = createScene(engine);
 
-    engine.runRenderLoop(function () {
-        teapotMesh.rotate(BABYLON.Axis.Y, -Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
-        scene.render();
+        engine.runRenderLoop(function () {
+            teapotMesh.rotate(BABYLON.Axis.Y, -Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+            scene.render();
+        });
+
+        window.addEventListener('resize', function(){
+            engine.resize();
+        });
     });
-});
+}
 
+init();
--- examples/babylonjs/texture/index.js
+++ examples/babylonjs/texture/index.js
@@ -1,154 +1,158 @@
-let cube;
-let createScene = function(engine) {
-    let scene = new BABYLON.Scene(engine);
-    let camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0, -3), scene);
-    cube = new BABYLON.Mesh('cube', scene);
-    scene.clearColor = new BABYLON.Color3(1, 1, 1);
+function init() {
+    const canvas = document.querySelector("#c");
+    const engine = new BABYLON.Engine(canvas);
+    let cube;
+    const createScene = function() {
+        const scene = new BABYLON.Scene(engine);
+        const camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0, -3), scene);
+        cube = new BABYLON.Mesh('cube', scene);
+        scene.clearColor = new BABYLON.Color3(1, 1, 1);
 
-    // Cube data
-    //             1.0 y 
-    //              ^  -1.0 
-    //              | / z
-    //              |/       x
-    // -1.0 -----------------> +1.0
-    //            / |
-    //      +1.0 /  |
-    //           -1.0
-    // 
-    //         [7]------[6]
-    //        / |      / |
-    //      [3]------[2] |
-    //       |  |     |  |
-    //       | [4]----|-[5]
-    //       |/       |/
-    //      [0]------[1]
-    //
-    let positions = [
+        // Cube data
+        //             1.0 y 
+        //              ^  -1.0 
+        //              | / z
+        //              |/       x
+        // -1.0 -----------------> +1.0
+        //            / |
+        //      +1.0 /  |
+        //           -1.0
+        // 
+        //         [7]------[6]
+        //        / |      / |
+        //      [3]------[2] |
+        //       |  |     |  |
+        //       | [4]----|-[5]
+        //       |/       |/
+        //      [0]------[1]
+        //
+        const positions = [
+                // Front face
+                -0.5, -0.5,  0.5, // v0
+                 0.5, -0.5,  0.5, // v1
+                 0.5,  0.5,  0.5, // v2
+                -0.5,  0.5,  0.5, // v3
+                // Back face
+                -0.5, -0.5, -0.5, // v4
+                 0.5, -0.5, -0.5, // v5
+                 0.5,  0.5, -0.5, // v6
+                -0.5,  0.5, -0.5, // v7
+                // Top face
+                 0.5,  0.5,  0.5, // v2
+                -0.5,  0.5,  0.5, // v3
+                -0.5,  0.5, -0.5, // v7
+                 0.5,  0.5, -0.5, // v6
+                // Bottom face
+                -0.5, -0.5,  0.5, // v0
+                 0.5, -0.5,  0.5, // v1
+                 0.5, -0.5, -0.5, // v5
+                -0.5, -0.5, -0.5, // v4
+                // Right face
+                 0.5, -0.5,  0.5, // v1
+                 0.5,  0.5,  0.5, // v2
+                 0.5,  0.5, -0.5, // v6
+                 0.5, -0.5, -0.5, // v5
+                // Left face
+                -0.5, -0.5,  0.5, // v0
+                -0.5,  0.5,  0.5, // v3
+                -0.5,  0.5, -0.5, // v7
+                -0.5, -0.5, -0.5  // v4
+        ];
+        const colors = [
+                1.0, 0.0, 0.0, 1.0, // Front face
+                1.0, 0.0, 0.0, 1.0, // Front face
+                1.0, 0.0, 0.0, 1.0, // Front face
+                1.0, 0.0, 0.0, 1.0, // Front face
+                1.0, 1.0, 0.0, 1.0, // Back face
+                1.0, 1.0, 0.0, 1.0, // Back face
+                1.0, 1.0, 0.0, 1.0, // Back face
+                1.0, 1.0, 0.0, 1.0, // Back face
+                0.0, 1.0, 0.0, 1.0, // Top face
+                0.0, 1.0, 0.0, 1.0, // Top face
+                0.0, 1.0, 0.0, 1.0, // Top face
+                0.0, 1.0, 0.0, 1.0, // Top face
+                1.0, 0.5, 0.5, 1.0, // Bottom face
+                1.0, 0.5, 0.5, 1.0, // Bottom face
+                1.0, 0.5, 0.5, 1.0, // Bottom face
+                1.0, 0.5, 0.5, 1.0, // Bottom face
+                1.0, 0.0, 1.0, 1.0, // Right face
+                1.0, 0.0, 1.0, 1.0, // Right face
+                1.0, 0.0, 1.0, 1.0, // Right face
+                1.0, 0.0, 1.0, 1.0, // Right face
+                0.0, 0.0, 1.0, 1.0, // Left face
+                0.0, 0.0, 1.0, 1.0, // Left face
+                0.0, 0.0, 1.0, 1.0, // Left face
+                0.0, 0.0, 1.0, 1.0  // Left face
+        ];
+        const uv = [
             // Front face
-            -0.5, -0.5,  0.5, // v0
-             0.5, -0.5,  0.5, // v1
-             0.5,  0.5,  0.5, // v2
-            -0.5,  0.5,  0.5, // v3
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
             // Back face
-            -0.5, -0.5, -0.5, // v4
-             0.5, -0.5, -0.5, // v5
-             0.5,  0.5, -0.5, // v6
-            -0.5,  0.5, -0.5, // v7
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
             // Top face
-             0.5,  0.5,  0.5, // v2
-            -0.5,  0.5,  0.5, // v3
-            -0.5,  0.5, -0.5, // v7
-             0.5,  0.5, -0.5, // v6
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
             // Bottom face
-            -0.5, -0.5,  0.5, // v0
-             0.5, -0.5,  0.5, // v1
-             0.5, -0.5, -0.5, // v5
-            -0.5, -0.5, -0.5, // v4
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
             // Right face
-             0.5, -0.5,  0.5, // v1
-             0.5,  0.5,  0.5, // v2
-             0.5,  0.5, -0.5, // v6
-             0.5, -0.5, -0.5, // v5
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
             // Left face
-            -0.5, -0.5,  0.5, // v0
-            -0.5,  0.5,  0.5, // v3
-            -0.5,  0.5, -0.5, // v7
-            -0.5, -0.5, -0.5  // v4
-    ];
-    let colors = [
-            1.0, 0.0, 0.0, 1.0, // Front face
-            1.0, 0.0, 0.0, 1.0, // Front face
-            1.0, 0.0, 0.0, 1.0, // Front face
-            1.0, 0.0, 0.0, 1.0, // Front face
-            1.0, 1.0, 0.0, 1.0, // Back face
-            1.0, 1.0, 0.0, 1.0, // Back face
-            1.0, 1.0, 0.0, 1.0, // Back face
-            1.0, 1.0, 0.0, 1.0, // Back face
-            0.0, 1.0, 0.0, 1.0, // Top face
-            0.0, 1.0, 0.0, 1.0, // Top face
-            0.0, 1.0, 0.0, 1.0, // Top face
-            0.0, 1.0, 0.0, 1.0, // Top face
-            1.0, 0.5, 0.5, 1.0, // Bottom face
-            1.0, 0.5, 0.5, 1.0, // Bottom face
-            1.0, 0.5, 0.5, 1.0, // Bottom face
-            1.0, 0.5, 0.5, 1.0, // Bottom face
-            1.0, 0.0, 1.0, 1.0, // Right face
-            1.0, 0.0, 1.0, 1.0, // Right face
-            1.0, 0.0, 1.0, 1.0, // Right face
-            1.0, 0.0, 1.0, 1.0, // Right face
-            0.0, 0.0, 1.0, 1.0, // Left face
-            0.0, 0.0, 1.0, 1.0, // Left face
-            0.0, 0.0, 1.0, 1.0, // Left face
-            0.0, 0.0, 1.0, 1.0  // Left face
-    ];
-    let uv = [
-        // Front face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        // Back face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        // Top face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        // Bottom face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        // Right face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        // Left face
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1
-    ];
-    let indices = [
-         0,  1,  2,    0,  2 , 3,  // Front face
-         4,  5,  6,    4,  6 , 7,  // Back face
-         8,  9, 10,    8, 10, 11,  // Top face
-        12, 13, 14,   12, 14, 15,  // Bottom face
-        16, 17, 18,   16, 18, 19,  // Right face
-        20, 21, 22,   20, 22, 23   // Left face
-    ];
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1
+        ];
+        const indices = [
+             0,  1,  2,    0,  2 , 3,  // Front face
+             4,  5,  6,    4,  6 , 7,  // Back face
+             8,  9, 10,    8, 10, 11,  // Top face
+            12, 13, 14,   12, 14, 15,  // Bottom face
+            16, 17, 18,   16, 18, 19,  // Right face
+            20, 21, 22,   20, 22, 23   // Left face
+        ];
 
-    cube.setVerticesData(BABYLON.VertexBuffer.PositionKind, positions, true);
-    cube.setVerticesData(BABYLON.VertexBuffer.UVKind, uv);
-    cube.setIndices(indices);
+        cube.setVerticesData(BABYLON.VertexBuffer.PositionKind, positions, true);
+        cube.setVerticesData(BABYLON.VertexBuffer.UVKind, uv);
+        cube.setIndices(indices);
 
-    let material = new BABYLON.ShaderMaterial("material", scene, {
-        vertexElement: "vs",
-        fragmentElement: "fs",
-    }, {
-        attributes: ["position", "uv"],
-        uniforms: ["worldViewProjection"]
+        const material = new BABYLON.ShaderMaterial("material", scene, {
+            vertexElement: "vs",
+            fragmentElement: "fs",
+        }, {
+            attributes: ["position", "uv"],
+            uniforms: ["worldViewProjection"]
+        });
+        
+        material.setTexture("texture_", new BABYLON.Texture("../../../assets/textures/frog.jpg", scene)); // 256x256
+
+        cube.material = material;
+        cube.material.backFaceCulling = false;
+
+        return scene;
+    };
+
+    const scene = createScene();
+
+    engine.runRenderLoop(function () {
+        cube.rotate(BABYLON.Axis.X, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+        cube.rotate(BABYLON.Axis.Y, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+        cube.rotate(BABYLON.Axis.Z, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+        scene.render();
     });
-    
-    material.setTexture("texture_", new BABYLON.Texture("../../../assets/textures/frog.jpg", scene)); // 256x256
+}
 
-    cube.material = material;
-    cube.material.backFaceCulling = false;
-
-    return scene;
-};
-
-let canvas = document.querySelector("#c");
-let engine = new BABYLON.Engine(canvas, true);
-let scene = createScene(engine);
-
-engine.runRenderLoop(function () {
-    cube.rotate(BABYLON.Axis.X, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
-    cube.rotate(BABYLON.Axis.Y, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
-    cube.rotate(BABYLON.Axis.Z, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
-    scene.render();
-});
+init();
