8accb75b27c273212c25096ecab2eacba10b9dab
cx20 <y-shimano@hondarer-soft.com>
2022-05-29 02:42:23 +0900

    Update three.js r140 to r141

--- examples/threejs/complex/index.js
+++ examples/threejs/complex/index.js
@@ -1,6 +1,6 @@
-//import * as THREE from 'https://cx20.github.io/gltf-test/libs/three.js/r140/build/three.module.js';
-//import { OrbitControls } from 'https://cx20.github.io/gltf-test/libs/three.js/r140/examples/jsm/controls/OrbitControls.js';
-//import { GLTFLoader } from 'https://cx20.github.io/gltf-test/libs/three.js/r140/examples/jsm/loaders/GLTFLoader.js';
+//import * as THREE from 'https://cx20.github.io/gltf-test/libs/three.js/r141/build/three.module.js';
+//import { OrbitControls } from 'https://cx20.github.io/gltf-test/libs/three.js/r141/examples/jsm/controls/OrbitControls.js';
+//import { GLTFLoader } from 'https://cx20.github.io/gltf-test/libs/three.js/r141/examples/jsm/loaders/GLTFLoader.js';
 
 let modelInfoSet = [
 {
