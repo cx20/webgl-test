4e762c75b3eec0002fd9eac28cd32e794c8b88ea
cx20 <y-shimano@hondarer-soft.com>
2022-06-25 15:22:57 +0900

    Improved Babylon.js sample to use MeshBuilder

--- examples/babylonjs/primitive/index.js
+++ examples/babylonjs/primitive/index.js
@@ -10,36 +10,28 @@ const createScene = function() {
 
     scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
-    // CreatePlane(name, size, scene, updatable, sideOrientation)
-    const plane = BABYLON.Mesh.CreatePlane('plane', 1.0, scene);
+    const plane = BABYLON.MeshBuilder.CreatePlane('plane', {size: 1.0}, scene);
     plane.position = new BABYLON.Vector3(-1.5, 1.5, 0); 
 
-    // CreateBox(name, size, scene, updatable, sideOrientation)
-    const cube = BABYLON.Mesh.CreateBox('cube', 1.0, scene);
+    const cube = BABYLON.MeshBuilder.CreateBox('cube', {size: 1.0}, scene);
     cube.position = new BABYLON.Vector3(0, 1.5, 0); 
 
-    // CreateSphere(name, segments, diameter, scene, updatable, sideOrientation)
-    const sphere = BABYLON.Mesh.CreateSphere('sphere', 24.0, 1.0, scene);
+    const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {segments: 24.0, diameter: 1.0}, scene);
     sphere.position = new BABYLON.Vector3(1.5, 1.5, 0); 
     
-    // CreateDisc(name, radius, tessellation, scene, updatable, sideOrientation)
-    const circle = BABYLON.Mesh.CreateDisc("disc", 0.5, 24, scene);
+    const circle = BABYLON.MeshBuilder.CreateDisc("disc", {radius: 0.5, tessellation: 24}, scene);
     circle.position = new BABYLON.Vector3(-1.5, 0, 0); 
     
-    // CreateCylinder(name, height, diameterTop, diameterBottom, tessellation, subdivisions, scene, updatable, sideOrientation)
-    const cylinder = BABYLON.Mesh.CreateCylinder("Cylinder", 1, 1, 1, 32, scene);
+    const cylinder = BABYLON.MeshBuilder.CreateCylinder("Cylinder", {height: 1, diameterTop: 1, diameterBottom: 1, tessellation: 32}, scene);
     cylinder.position = new BABYLON.Vector3(0, 0, 0); 
 
-    // CreateCylinder(name, height, diameterTop, diameterBottom, tessellation, subdivisions, scene, updatable, sideOrientation)
-    const cone = BABYLON.Mesh.CreateCylinder("Cone", 1, 0, 1, 32, scene);
+    const cone = BABYLON.MeshBuilder.CreateCylinder("Cone", {height: 1, diameterTop: 0, diameterBottom: 1, tessellation: 32}, scene);
     cone.position = new BABYLON.Vector3(1.5, 0, 0); 
     
-    // CreateTorusKnot(name, radius, tube, radialSegments, tubularSegments, p, q, scene, updatable, sideOrientation)
-    const knot = BABYLON.Mesh.CreateTorusKnot("knot", 0.3, 0.1, 128, 64, 2, 3, scene);
+    const knot = BABYLON.MeshBuilder.CreateTorusKnot("knot", {radius: 0.3, tube: 0.1, radialSegments: 128, tubularSegments: 64, p: 2, q: 3}, scene);
     knot.position = new BABYLON.Vector3(-1.5, -1.5, 0); 
 
-    // CreateTorus(name, diameter, thickness, tessellation, scene, updatable, sideOrientation)
-    const torus = BABYLON.Mesh.CreateTorus("torus", 1.0, 0.2, 10, scene);
+    const torus = BABYLON.MeshBuilder.CreateTorus("torus", {diameter: 1.0, thickness: 0.2, tessellation: 10}, scene);
     torus.position = new BABYLON.Vector3(0, -1.5, 0); 
     
     const octa = BABYLON.MeshBuilder.CreatePolyhedron("oct", {type: 1, size: 0.5}, scene);
