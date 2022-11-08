5a8f0a0e38991652ba7344ccef078aafa0702d61
cx20 <y-shimano@hondarer-soft.com>
2022-11-09 05:51:11 +0900

    Update Filament v1.28.0 to v1.28.2

---
+++ examples/filament/matc_all.bat
@@ -0,0 +1,6 @@
+REM This batch file is a batch file for recompiling Filament material files.
+
+SET EXAMPLES=triangle cube square texture teapot
+FOR %%a IN (%EXAMPLES%) DO (
+	matc -o %%a\%%a.filamat %%a\%%a.mat
+)
