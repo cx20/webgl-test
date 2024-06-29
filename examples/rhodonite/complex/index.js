24e3da3628cfb677280f49696e56e1c65201557f
cx20 <y-shimano@hondarer-soft.com>
2024-06-29 22:58:46 +0900

    Fixed the issue of the screen going completely black in Rhodonite

--- examples/rhodonite/complex/index.js
+++ examples/rhodonite/complex/index.js
@@ -30,8 +30,8 @@ c.width = window.innerWidth;
 c.height = window.innerHeight;
 
 const load = async function () {
-  Rn.Config.dataTextureWidth  = 2 ** 11; // default: 2 ** 11;
-  Rn.Config.dataTextureHeight = 2 ** 11; // default: 2 ** 11;
+  Rn.Config.dataTextureWidth  = 2 ** 9; // default: 2 ** 11;
+  Rn.Config.dataTextureHeight = 2 ** 9; // default: 2 ** 11;
 
   await Rn.ModuleManager.getInstance().loadModule('webgl');
   await Rn.ModuleManager.getInstance().loadModule('pbr');
@@ -67,18 +67,18 @@ const load = async function () {
   const lightComponent1 = lightEntity1.getLight();
   lightComponent1.type = Rn.LightType.Directional;
   lightEntity1.getTransform().localPosition = Rn.Vector3.fromCopyArray([1.0, 1.0, 100000.0]);
-  lightEntity1.getComponent(Rn.LightComponent).intensity = Rn.Vector3.fromCopyArray([0, 0, 0]);
+  lightEntity1.getComponent(Rn.LightComponent).intensity = Rn.Vector3.fromCopyArray([1, 1, 1]);
   lightEntity1.getComponent(Rn.LightComponent).type = Rn.LightType.Directional;
-  lightEntity1.getTransform().localEulerAngles = Rn.Vector3.fromCopyArray([-Math.PI / 2, -Math.PI / 4, Math.PI / 4]);
-
+  lightEntity1.getTransform().localEulerAngles = Rn.Vector3.fromCopyArray([Math.PI / 2, Math.PI / 4, Math.PI / 4]);
+/*
   const lightEntity2 = Rn.EntityHelper.createLightEntity();
   const lightComponent2 = lightEntity2.getLight();
   lightComponent1.type = Rn.LightType.Directional;
   lightEntity2.getTransform().localPosition = Rn.Vector3.fromCopyArray([1.0, 1.0, 100000.0]);
-  lightEntity2.getComponent(Rn.LightComponent).intensity = Rn.Vector3.fromCopyArray([0, 0, 0]);
+  lightEntity2.getComponent(Rn.LightComponent).intensity = Rn.Vector3.fromCopyArray([1, 1, 1]);
   lightEntity2.getComponent(Rn.LightComponent).type = Rn.LightType.Directional;
   lightEntity2.getTransform().localEulerAngles = Rn.Vector3.fromCopyArray([Math.PI / 2, Math.PI / 4, -Math.PI / 4]);
-
+*/
   // expressions
   const expressions = [];
 
@@ -92,7 +92,7 @@ const load = async function () {
           },
         ]
       })
-	).unwrapForce();
+    ).unwrapForce();
     promises.push(promise);
   }
   
@@ -106,10 +106,8 @@ const load = async function () {
       rootGroup.getTransform().localEulerAngles = Rn.Vector3.fromCopyArray([modelInfo.rotation[0], modelInfo.rotation[1], modelInfo.rotation[2]]);
       rootGroup.getTransform().localPosition = Rn.Vector3.fromCopyArray([modelInfo.position[0], modelInfo.position[1], modelInfo.position[2]]);
 
-      //if (modelInfo.name == "Fox") {
       if (modelInfo.name == "Rex") {
         cameraControllerComponent.controller.setTarget(rootGroup);
-        //const cameraEntity = Rn.EntityHelper.createCameraControllerEntity();
         const cameraEntity = Rn.EntityHelper.createCameraEntity();
         const cameraComponent = cameraEntity.getCamera();
         cameraComponent.zNear = 0.1;
@@ -125,76 +123,25 @@ const load = async function () {
     renderPass.toClearColorBuffer = true;
     renderPass.toClearDepthBuffer = true;
     renderPass.clearColor = Rn.Vector4.fromCopyArray4([0.2, 0.2, 0.2, 1]);
-    
-    // TODO: 下記処理を通すと画面が真っ暗になる為、要調査
-/*
+
     // gamma correction
     const gammaTargetFramebuffer = Rn.RenderableHelper.createTexturesForRenderTarget(1024, 1024, 1, {});
     renderPass.setFramebuffer(gammaTargetFramebuffer);
 
-    const gammaRenderPass = createPostEffectRenderPass('createGammaCorrectionMaterial');
-    setTextureParameterForMeshComponents(gammaRenderPass.meshComponents, Rn.ShaderSemantics.BaseColorTexture, gammaTargetFramebuffer.colorAttachments[0]);
-*/
+    const gammaCorrectionMaterial = Rn.MaterialHelper.createGammaCorrectionMaterial();
+    const gammaRenderPass =
+    Rn.RenderPassHelper.createScreenDrawRenderPassWithBaseColorTexture(
+    	gammaCorrectionMaterial,
+    	gammaTargetFramebuffer.getColorAttachedRenderTargetTexture(0)
+    );
+
     const expression = new Rn.Expression();
-    //expression.addRenderPasses([renderPass, gammaRenderPass]);
-    expression.addRenderPasses([renderPass]);
+    expression.addRenderPasses([renderPass, gammaRenderPass]);
     expressions.push(expression);
   
     draw();
   });
 
-  function createPostEffectRenderPass(materialHelperFunctionStr, arrayOfHelperFunctionArgument = []) {
-    const boardPrimitive = new Rn.Plane();
-    boardPrimitive.generate({
-      width: 1, height: 1, uSpan: 1, vSpan: 1, isUVRepeat: false,
-      material: Rn.MaterialHelper[materialHelperFunctionStr].apply(this, arrayOfHelperFunctionArgument)
-    });
-  
-    const boardEntity = Rn.EntityHelper.createMeshEntity();
-    boardEntity.getTransform().localEulerAngles = Rn.Vector3.fromCopyArray([Math.PI / 2, 0.0, 0.0]);
-    boardEntity.getTransform().localPosition = Rn.Vector3.fromCopyArray([0.0, 0.0, -0.5]);
-  
-    const boardMesh = new Rn.Mesh();
-    boardMesh.addPrimitive(boardPrimitive);
-    const boardMeshComponent = boardEntity.getComponent(Rn.MeshComponent);
-    boardMeshComponent.setMesh(boardMesh);
-  
-    if (createPostEffectRenderPass.cameraComponent == null) {
-      const cameraEntity = Rn.EntityHelper.createCameraEntity();
-      const cameraComponent = cameraEntity.getCamera();
-      cameraComponent.zFarInner = 1.0;
-      createPostEffectRenderPass.cameraComponent = cameraComponent;
-    }
-  
-    const renderPass = new Rn.RenderPass();
-    renderPass.toClearColorBuffer = true;
-    renderPass.clearColor = Rn.Vector4.fromCopyArray4([0.0, 0.0, 0.0, 1.0]);
-    renderPass.cameraComponent = createPostEffectRenderPass.cameraComponent;
-    renderPass.addEntities([boardEntity]);
-  
-    return renderPass;
-  }
-
-  function setTextureParameterForMeshComponents(meshComponents, shaderSemantic, value) {
-    const sampler = new Rn.Sampler({
-      magFilter: Rn.TextureParameter.Linear,
-      minFilter: Rn.TextureParameter.Linear,
-      wrapS: Rn.TextureParameter.ClampToEdge,
-      wrapT: Rn.TextureParameter.ClampToEdge,
-    });
-    sampler.create();
-    
-    for (let i = 0; i < meshComponents.length; i++) {
-      const mesh = meshComponents[i].mesh;
-      if (!mesh) continue;
-  
-      const primitiveNumber = mesh.getPrimitiveNumber();
-      for (let j = 0; j < primitiveNumber; j++) {
-        const primitive = mesh.getPrimitiveAt(j);
-        primitive.material.setTextureParameter(shaderSemantic, value, sampler);
-      }
-    }
-  }
   
   let startTime = Date.now();
   const draw = function () {
